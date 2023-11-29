9b9d478faeda40d4fbcc5416e7cdfeca8e0a3b37
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-11-28 22:45:00 -0300

    create FilmeController

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new();
+
+    [HttpPost] //             Aqui é indicado de que o parâmetro virá do corpo da requisição
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}

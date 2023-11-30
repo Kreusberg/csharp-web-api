a5dcdc1fac0a1722a78a06d42bcaa996fc9cdac2
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-11-29 22:50:22 -0300

    create Get methods in FilmeController

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -8,12 +8,27 @@ namespace FilmesApi.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new();
+    private static int id = 0;
 
     [HttpPost] //             Aqui é indicado de que o parâmetro virá do corpo da requisição
     public void AdicionaFilme([FromBody] Filme filme)
     {
+
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 }

5f2f71044387f22c55ae7e3582e4842cf76de5b5
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-12-01 22:19:51 -0300

    add _context in FilmeController

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -7,15 +8,20 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new();
-    private static int id = 0;
+
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost] //             Aqui é indicado de que o parâmetro virá do corpo da requisição
     public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
-
-        filme.Id = id++;
-        filmes.Add(filme); //                              parâmetros necessários para retornar este valor
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+         //                              parâmetros necessários para retornar este valor
         return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id}, filme);
 //                          método executado para retornar este valor ||||       objeto criado         
     }
@@ -23,13 +29,13 @@ public class FilmeController : ControllerBase
     [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
 
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
         if (filme == null) return NotFound();
         return Ok(filme);

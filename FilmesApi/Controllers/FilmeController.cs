7276ecf5352357986210608d0373cf2532434bb8
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-12-02 15:13:51 -0300

    change RecuperaFilmes and RecuperaFilmePorId returns

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -33,9 +33,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -44,7 +44,10 @@ public class FilmeController : ControllerBase
         var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
         if (filme == null) return NotFound();
-        return Ok(filme);
+        
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+        
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]

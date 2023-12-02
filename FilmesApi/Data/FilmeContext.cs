530b45e9f49bc7ac6b0a25d585a80efe42a1d137
Lucas Lodi Kreusberg <kreusberglucas@gmail.com>
2023-12-01 21:26:20 -0300

    create database connection

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,17 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+        
+    }
+
+//  esta propriedade nos dará o acesso aos dados  
+    public DbSet<Filme> Filmes { get; set; }
+
+}
